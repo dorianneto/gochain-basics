aebd9f17cc481c9759931a6b68d7ad42d37afdca
Dorian <dorian@heyorca.com>
2022-12-16 19:30:43 -0330

    feat: generate block and calculate hash

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"time"
+)
+
 var BlockChain []Block
 
 type Block struct {
@@ -8,6 +14,29 @@ type Block struct {
 	BPM       int
 	Hash      string
 	PrevHash  string
+}
+
+func generateBlock(oldBlock Block, BPM int) (Block, error) {
+	var newBlock Block
+
+	timestamp := time.Now()
+
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = timestamp.String()
+	newBlock.BPM = BPM
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+
+	return newBlock, nil
+}
+
+func calculateHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
+
+	hash := sha256.New()
+	hash.Write([]byte(record))
+
+	return hex.EncodeToString(hash.Sum(nil))
 }
 
 func main() {}
